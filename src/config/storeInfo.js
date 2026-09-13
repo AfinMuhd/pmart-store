b95a53e9784fa5e29a82cae3cf8a102934348c1f
Aitron Devs <aitron.devs@gmail.com>
2026-09-13 11:34:58 +0530

    update about

--- src/config/storeInfo.js
+++ src/config/storeInfo.js
@@ -1,10 +1,10 @@
 export default {
   name: 'Punnamoottil Super Market',
-  tagline: 'Fresh groceries for every Kerala home',
-  shortName: 'Punnamood',
-  addressLines: ['NH 66, Punnamood Junction', 'Alappuzha, Kerala 688001'],
-  phone: '+91 477 123 4567',
-  email: 'hello@punnamoodsupermarket.in',
+  tagline: 'Fresh groceries for every home',
+  shortName: 'Punnamoottil',
+  addressLines: ['NH 66, Punnamoottil Junction', 'Koodal, Pathanamthitta, Kerala 689694'],
+  phone: '+91 974 541 9990',
+  email: '',
   hours: 'Open daily 7:00 AM – 9:30 PM',
-  gstin: '32AABCP0000P1Z5',
+  gstin: '',
 }
